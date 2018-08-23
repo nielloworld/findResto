24119209eafec9e73b84f752671cfb33b3fb6eaf
raniel <raniel.one@gmail.com>
2018-08-23 21:13:12 +0800

    search functionality/draw/types

--- js/load.js
+++ js/load.js
@@ -1,7 +1,12 @@
 
 $('.sidebar')
-.sidebar('attach events', '.open.button', 'show')
-.sidebar('setting', 'onShow', function() {})
-// second call swaps in setting without re-init
-.sidebar('setting', 'dimPage', false)
+  .sidebar('attach events', '.open.button', 'toggle')
+  .sidebar('setting', 'onShow', function() {})
+  // second call swaps in setting without re-init
+  .sidebar('setting', 'dimPage', false)
+  
 ;
+
+function getDirection(){
+    console.log("direction");
+}
--- js/map.js
+++ js/map.js
@@ -1,114 +1,149 @@
 // Initialize and add the map
-
-
-
+var cebuMap = {lat:  10.309576, lng: 123.893056};
 var map;
-
+var markers = [];
+var searchmarkers = [];
+var all_overlays = [];
 function initMap() {
   // Create the map.
-  var cebuMap = {lat:  10.309576, lng: 123.893056};
+  
   map = new google.maps.Map(document.getElementById('map'), {
     center: cebuMap,
-    zoom: 15
+    zoom: 12
   });
 
-  var input = document.getElementById('pac-input');
-  var types = "Establishment";
-  var autocomplete = new google.maps.places.Autocomplete(input);
+  //searchMap
+  searchMap();
+  //showRestaurants
+  showRestaurants('Restaurant');
+  //drawCapability
+  drawingManager();
+  
+}
 
-  autocomplete.bindTo('bounds', map);
-
-  autocomplete.setFields(
-    ['address_components', 'geometry', 'icon', 'name']);
-
-    var infowindow = new google.maps.InfoWindow();
-    var infowindowContent = document.getElementById('infowindow-content');
-    infowindow.setContent(infowindowContent);
-    var marker = new google.maps.Marker({
-    map: map,
-    anchorPoint: new google.maps.Point(0, -29)
-    });
-
-autocomplete.addListener('place_changed', function() {
-  infowindow.close();
-  marker.setVisible(false);
-  var place = autocomplete.getPlace();
-  if (!place.geometry) {
-    // User entered the name of a Place that was not suggested and
-    // pressed the Enter key, or the Place Details request failed.
-    window.alert("No details available for input: '" + place.name + "'");
-    return;
+function generateInfoWindowContent(content){
+ if ( content.opening_hours && typeof content.opening_hours != "undefined"){
+  var branchopen = "Closed";
+  if (content.opening_hours.open_now == true){
+    branchopen = "Open";
   }
+  return `<div class='container'><b>`+ content.name +`</b><br /> address: `+ content.vicinity +`<br /> rating: ` + content.rating +`<br /> ` + branchopen + `</div>` 
 
-  // If the place has a geometry, then present it on a map.
-  if (place.geometry.viewport) {
-    map.fitBounds(place.geometry.viewport);
-  } else {
-    map.setCenter(place.geometry.location);
-    map.setZoom(17);  // Why 17? Because it looks good.
+ }else{
+  return `<div class='container'><b>`+ content.name +`</b><br /> address: `+ content.vicinity +`<br /> rating: ` + content.rating + `</div>` 
+
+ }
+
+}
+
+function generateSideBarcontent(content){
+  console.log("start");
+  console.log(content);
+  console.log("wwe---------------");
+  if ( content.opening_hours && typeof content.opening_hours != "undefined"){
+    var branchopen = "N/A";
+    if (content.opening_hours.open_now == true){
+      branchopen = "Open";
+    }else{
+      branchopen = "Closed";
+    }
   }
-  marker.setPosition(place.geometry.location);
-  marker.setVisible(true);
+  var sideDetails = document.getElementById('sideDetails');
+  sideDetails.classList.add("ui");
+  sideDetails.classList.add("card");
+  sideDetails.innerHTML = `
+  <div class='content'>
+    <div class='header'><b>`+content.name  +`</b></div>  
+    <div class="description">
+      <p>address:<b> `+ content.formatted_address +`</b></p>
+      <p>phone: <b>`+ content.formatted_phone_number +`</b></p>
+      <p>rating: <b>`+ content.rating +`</b></p>
+      <p><b>`+ branchopen +`</b></p>
+      <hr>
+      <p>website: <b>`+ content.website +`</b></p>
+    </div>
+  </div> 
+  <div class="extra content">
+    <div class="right floated author">
+      <button class ="ui button"  id="getDirections">get directions</button>
+    </div>
+  </div>
+  </div>`
+  bindingFunction();
+  
+ 
+}
 
-  var address = '';
-  if (place.address_components) {
-    address = [
-      (place.address_components[0] && place.address_components[0].short_name || ''),
-      (place.address_components[1] && place.address_components[1].short_name || ''),
-      (place.address_components[2] && place.address_components[2].short_name || '')
-    ].join(' ');
+function bindingFunction(){
+  document.getElementById('getDirections').onclick = function() {
+//     Your code
+console.log("direction");
   }
+}
+function changeContent(){
+  $('.sidebar')
+  .sidebar( 'show')
+}
+function closableContent(){
+  $('.sidebar')
+  .sidebar( 'show')
+  .sidebar('setting', 'closable', false)
+}
 
-  infowindowContent.children['place-icon'].src = place.icon;
-  infowindowContent.children['place-name'].textContent = place.name;
-  infowindowContent.children['place-address'].textContent = address;
-  infowindow.open(map, marker);
-});
+function hideContent(){
+  $('.sidebar')
+  .sidebar('hide')
+}
+function markerFunctions(marker, infowindow, content){
+  var infocontent = generateInfoWindowContent(content);
+  
+  marker.addListener('mouseover', function() {
+    infowindow.setContent(infocontent);
+    infowindow.open(map, this);
+  });
 
+  marker.addListener('mouseout', function() {
+      infowindow.close();
+  });
 
-  // Create the places service.
-  var service = new google.maps.places.PlacesService(map);
-  var getNextPage = null;
-  var moreButton = document.getElementById('more');
- // moreButton.onclick = function() {
- //   moreButton.disabled = true;
- //   if (getNextPage) getNextPage();
- // };
-
-  // Perform a nearby search.
-  service.nearbySearch(
-      {location: cebuMap, radius: 5000, type: ['restaurant']},
-      function(results, status, pagination) {
-        if (status !== 'OK') return;
-
-        createMarkers(results);
-        //moreButton.disabled = !pagination.hasNextPage;
-        //getNextPage = pagination.hasNextPage && function() {
-        //  pagination.nextPage();
-        //};  
-      });
+  marker.addListener('click', function(){
+    closableContent()
+    searchMapViaId(content.place_id, function(trueContent){
+     
+    })
+  });
 }
 
 function createMarkers(places) {
   var bounds = new google.maps.LatLngBounds();
   var placesList = document.getElementById('places');
-
+  var urlicon="";
   for (var i = 0, place; place = places[i]; i++) {
+    if (place.types.includes('cafe')){
+      urlicon='https://image.flaticon.com/icons/svg/35/35101.svg'
+    }else{
+      urlicon='https://i1.wp.com/hentiesbaytourism.com/wp-content/uploads/2016/06/restaurant_marker.png'
+    }
     var image = {
-      url: place.icon,
-      size: new google.maps.Size(71, 71),
-      origin: new google.maps.Point(0, 0),
-      anchor: new google.maps.Point(17, 34),
-      scaledSize: new google.maps.Size(25, 25)
-    };
+      url: urlicon,
 
+      scaledSize: new google.maps.Size(45, 45)
+    };
+    var infowindow = new google.maps.InfoWindow();
+    //var infowindowContent = document.getElementById('infowindow-content');
+
+    //infowindow.setContent(infowindowContent);
     var marker = new google.maps.Marker({
       map: map,
       icon: image,
-      title: place.name,
+      class:'open button',
       position: place.geometry.location
     });
-
+    marker.addListener('click', changeContent);
+    //populateData(infowindowContent,place.icon,place.name,place.address)  
+    
+    markerFunctions(marker, infowindow, place);
+    markers.push(marker);
     var li = document.createElement('li');
     li.textContent = place.name;
     placesList.appendChild(li);
@@ -117,3 +152,230 @@ function createMarkers(places) {
   }
   map.fitBounds(bounds);
 }
+
+
+
+function showRestaurants(rtype){
+  hideContent();
+  cleanrightBar();
+  deleteMarkers(markers);
+  var rightHeader = document.getElementById('rightHeader');
+  rightHeader.innerHTML = rtype;
+  rtype = rtype.toLowerCase();
+  // Create the places service.
+  var service = new google.maps.places.PlacesService(map);
+  var getNextPage = null;
+  var moreButton = document.getElementById('more');
+  moreButton.onclick = function() {
+    moreButton.disabled = true;
+    if (getNextPage) getNextPage();
+  };
+
+  // Perform a nearby search.
+  service.nearbySearch(
+      {location: cebuMap, radius: 5000, type: [rtype]},
+      function(results, status, pagination) {
+        if (status !== 'OK') return;
+        createMarkers(results);
+        moreButton.disabled = !pagination.hasNextPage;
+        getNextPage = pagination.hasNextPage && function() {
+          pagination.nextPage();
+        };  
+      });
+}
+
+function searchMapViaId(placeId){
+  var service = new google.maps.places.PlacesService(map);
+  console.log("searchMapViaId ==============================")
+  console.log(placeId)
+  service.getDetails({
+    placeId: placeId
+  }, function(place, status) {
+    if (status === google.maps.places.PlacesServiceStatus.OK) {
+      //var marker = new google.maps.Marker({
+      //  map: map,
+      //  position: place.geometry.location
+      //});
+      console.log("start==========================")
+      console.log(place)
+      generateSideBarcontent(place);
+    }
+  });
+}
+
+function searchMap(){
+    var input = document.getElementById('pac-input');
+    var types = "Restaurant";
+    var autocomplete = new google.maps.places.Autocomplete(input);
+  
+    autocomplete.bindTo('bounds', map);
+  
+      var infowindow = new google.maps.InfoWindow();
+      var infowindowContent = document.getElementById('infowindow-content');
+      infowindow.setContent(infowindowContent);
+      var marker = new google.maps.Marker({
+      map: map,
+      class:'open button',
+      anchorPoint: new google.maps.Point(0, -29)
+      });
+      
+    autocomplete.addListener('place_changed', function() {
+    infowindow.close();
+    clearMarkers(markers);
+    marker.setVisible(false);
+    var place = autocomplete.getPlace();
+    if (!place.geometry) {
+      // User entered the name of a Place that was not suggested and
+      // pressed the Enter key, or the Place Details request failed.
+      window.alert("No details available for input: '" + place.name + "'");
+      return;
+    }
+   
+    // If the place has a geometry, then present it on a map.
+    if (place.geometry.viewport) {
+      map.fitBounds(place.geometry.viewport);
+    } else {
+      map.setCenter(place.geometry.location);
+      map.setZoom(17);  
+    }
+    marker.setPosition(place.geometry.location);
+    marker.setVisible(true);
+    searchmarkers.push(marker);
+    showMarkers(searchmarkers);
+    var address = '';
+    if (place.address_components) {
+      address = [
+        (place.address_components[0] && place.address_components[0].short_name || ''),
+        (place.address_components[1] && place.address_components[1].short_name || ''),
+        (place.address_components[2] && place.address_components[2].short_name || '')
+      ].join(' ');
+    }
+    //populateData(infowindowContent,place.icon,place.name,address)    
+    //infowindow.open(map, marker);
+    searchMapViaId(place.place_id, function(trueContent){
+      
+    })
+    markerFunctions(marker, infowindow, place);
+
+  });
+  infowindow.addListener('closeclick', function(){
+    deleteMarkers(searchmarkers);
+    showMarkers(markers);
+
+  });
+}
+
+function populateData(infowindowContent,picon, pname,paddr){
+    infowindowContent.children['place-icon'].src = picon;
+    infowindowContent.children['place-name'].textContent = pname;
+    infowindowContent.children['place-address'].textContent = paddr;
+}
+
+// Sets the map on all markers in the array.
+function setMapOnAll(map, markerArray) {
+  for (var i = 0; i < markerArray.length; i++) {
+    markerArray[i].setMap(map);
+  }
+}
+
+// Removes the markers from the map, but keeps them in the array.
+function clearMarkers(markertoHide) {
+  setMapOnAll(null, markertoHide);
+}
+
+// Shows any markers currently in the array.
+function showMarkers(markerstoShow) {
+  setMapOnAll(map, markerstoShow);
+}
+
+// Deletes all markers in the array by removing references to them.
+function deleteMarkers(markertoDelete) {
+  if(markertoDelete.length > 0){
+    clearMarkers(markertoDelete);
+    markertoDelete = [];
+  }
+
+}
+
+function cleanrightBar(){
+    var element = document.getElementById('places');
+    //var element = document.getElementById("top");
+    while (element.firstChild) {
+      element.removeChild(element.firstChild);
+    }
+
+}
+
+function drawingManager(){
+  var drawingManager = new google.maps.drawing.DrawingManager({
+    drawingMode: google.maps.drawing.OverlayType.MARKER,
+    drawingControl: true,
+    drawingControlOptions: {
+      position: google.maps.ControlPosition.TOP_CENTER,
+      drawingModes: [ 'circle']
+    },
+    markerOptions: {icon: 'https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png'},
+    circleOptions: {
+      fillColor: '#000',
+      fillOpacity: .3,
+      strokeWeight: 1,
+      clickable: false,
+      editable: true,
+      zIndex: 1
+    }
+  });
+  drawingManager.setMap(map);
+  drawingManager.setDrawingMode(null);
+
+  google.maps.event.addListener(drawingManager, 'overlaycomplete', function(event) {
+    all_overlays.push(event);
+    if (event.type == 'circle') {
+      var radius = event.overlay.getRadius();
+      var lat = event.overlay.getCenter().lat();
+      var lng = event.overlay.getCenter().lng();
+
+    }else{
+      console.log(event.overlay.getMap().center.lat());
+      console.log(event.overlay.getMap().center.lng());
+    }
+    deleteAllShape();
+    drawingManager.setDrawingMode(null);
+    showRestaurantsDraw(radius,lat,lng);
+  });
+  
+}
+
+function deleteAllShape() {
+  for (var i = 0; i < all_overlays.length; i++) {
+    all_overlays[i].overlay.setMap(null);
+  }
+  all_overlays = [];
+}
+
+function showRestaurantsDraw(radius,lat,lng){
+  cleanrightBar();
+  deleteMarkers(markers);
+  var rightHeader = document.getElementById('rightHeader');
+  rightHeader.innerHTML = 'Restaurant';
+  // Create the places service.
+  var service = new google.maps.places.PlacesService(map);
+  var getNextPage = null;
+  var moreButton = document.getElementById('more');
+  moreButton.onclick = function() {
+    moreButton.disabled = true;
+    if (getNextPage) getNextPage();
+  };
+
+  // Perform a nearby search.
+  service.nearbySearch(
+      {location: {lat:  lat, lng: lng}, radius: radius, type: ['food']},
+      function(results, status, pagination) {
+        if (status !== 'OK') return;
+        createMarkers(results);
+        moreButton.disabled = !pagination.hasNextPage;
+        getNextPage = pagination.hasNextPage && function() {
+          pagination.nextPage();
+        };  
+      });
+}
+
